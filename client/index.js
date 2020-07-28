dcd82d4753990cdbc21df2ea7710a86a79d804e7
Henry Black <blackhaj@gmail.com>
2020-07-27 21:02:41 -0500

    Added async support

--- client/index.js
+++ client/index.js
@@ -4,6 +4,10 @@ import { render } from 'react-dom';
 import styles from './scss/application.scss';
 import App from './pages/App.js'
 
+// For async methods to work
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 render(
   <App/>,
   document.getElementById('root')
