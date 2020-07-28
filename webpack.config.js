05ae33c814bf34ee909851b5affa12d57a9e4dcc
Henry Black <blackhaj@gmail.com>
2020-07-27 20:59:11 -0500

    Added async suppor

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: ["@babel/plugin-proposal-class-properties"]
+            plugins: ["@babel/plugin-proposal-class-properties", '@babel/plugin-transform-runtime']
           },
         },
       },
