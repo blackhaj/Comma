1b36e900304fb558beac4f4ea1afa228b58d192d
Henry Black <blackhaj@gmail.com>
2020-07-28 21:58:27 -0500

    Renamed graph to cahrt

---
+++ client/components/Chart.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+
+export default function Chart(props) {
+  let output;
+  if (props.fetched){
+    let options = {
+      scales: {
+        yAxes: [{
+            ticks: {
+                suggestedMin: 400,
+
+            }
+        }]
+      },
+      legend:{
+        display:false
+      }
+    }
+    output = <Line data={props.data} options={options} />
+  } else {
+    output = <button class="button is-loading chart-loading">Loading</button>
+  }
+
+
+  return (
+    <div class='chart'>
+        {output}
+    </div>
+  )
+}
+
